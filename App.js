54ac85c05965551dcdb0c2b118900c9e4c89e40b
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-16 08:03:01 -0400

    implement image component with local and network images

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("Text Clicked");
-  }
   return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        This is a very long piece of text that will demonstrate how truncation
-        works in React Native.
-      </Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text>Hello React Native</Text>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
