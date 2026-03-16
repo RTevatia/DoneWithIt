e87ae5343888d39ea36dbd32ab81a657ca3e8444
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-16 18:32:51 -0400

    implement button

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableHighlight,
+  Button,
+  StyleSheet
 } from "react-native";
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <Text>Hello React Native</Text>
-        <TouchableHighlight onPress={() => console.log("Image Tapped")}>
-          <Image
-            source={{
-              width: 200,
-              height: 300,
-              uri: "https://picsum.photos/200/300",
-            }}
-          />
-        </TouchableHighlight>
-        <StatusBar style="auto" />
+        <Button
+          color="orange"
+          title="Click Me"
+          onPress={() => console.log("Button tapped")}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
