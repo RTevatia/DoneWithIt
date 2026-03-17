5ced32fab8234c974618c0796572bb8a669293a7
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-16 22:12:42 -0400

    add flex direction layout

--- App.js
+++ App.js
@@ -2,23 +2,26 @@ import { StatusBar, StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "dodgerblue", flex: 1 }}>
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection:"row" }}>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          flex: 2,
+          width: 100,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}
       />
     </View>
