3af5126491ae0a79d16383d64ea9f0178ae90033
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-18 23:03:25 -0400

    Create AppText component to encapsulate style

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+import AppText from "./app/components/AppText";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>I love React Native!</AppText>
+    </View>
+  );
 }
