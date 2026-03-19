667aded9182f39d0ba2fbfc3a29701ff11931378
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-19 19:24:17 -0400

    Apply AppButton on WelcomeScreen and polish layout

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 import { View } from "react-native";
-import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AppText>I love React Native!</AppText>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
