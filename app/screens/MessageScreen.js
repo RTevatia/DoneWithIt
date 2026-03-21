c7072434535daa410dcb54c90e8967497d0b7184
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-21 16:10:47 -0400

    Add TouchableHight to ListItem

--- app/screens/MessageScreen.js
+++ app/screens/MessageScreen.js
@@ -30,6 +30,7 @@ function MessageScreen(props) {
             title={item.title}
             subTitle={item.description}
             image={item.image}
+            onPress={() => console.log("Message selected", item)}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
