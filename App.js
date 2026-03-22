cb390942ca0dce8ffb363a952af24d2a418c8794
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-21 20:27:06 -0400

    Add Swipeable in ListItem with reusable delete action

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import MessageScreen from "./app/screens/MessageScreen";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
-  return <MessageScreen />;
+  return (
+    <GestureHandlerRootView>
+      <MessageScreen />
+    </GestureHandlerRootView>
+  );
 }
