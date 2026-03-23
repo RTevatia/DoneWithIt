c758e282c25e80c3b74bab3257b1f3987bd67029
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-22 22:31:48 -0400

    Refactor to centralize default text styles and colors in config/styles.js

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { TextInput, StyleSheet, View, Platform } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import colors from "../config/colors";
+
+import defaultStyles from "../config/styles";
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
@@ -10,7 +11,7 @@ function AppTextInput({ icon, ...otherProps }) {
         <MaterialCommunityIcons
           name={icon}
           size={20}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
@@ -21,7 +22,7 @@ function AppTextInput({ icon, ...otherProps }) {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
     width: "100%",
@@ -31,11 +32,7 @@ const styles = StyleSheet.create({
   icon: {
     marginRight: 10,
   },
-  textInput: {
-    color: colors.dark,
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-  },
+  textInput: defaultStyles.text,
 });
 
 export default AppTextInput;
