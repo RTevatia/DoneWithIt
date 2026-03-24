e9151edca370ae79b68cf117c4025db296968d18
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-23 21:39:23 -0400

    Implement Yup validation for email and password in LoginScreen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,12 +1,18 @@
 import { Formik } from "formik";
 import { Image, StyleSheet } from "react-native";
+import * as Yup from "yup";
 
 import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
+import AppText from "../components/AppText";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 function LoginScreen(props) {
-
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo-red.png")} />
@@ -14,8 +20,9 @@ function LoginScreen(props) {
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit }) => (
+        {({ handleChange, handleSubmit, errors }) => (
           <>
             <AppTextInput
               autoCapitalize="none"
@@ -26,6 +33,7 @@ function LoginScreen(props) {
               placeholder="Email"
               textContentType="emailAddress"
             />
+            <AppText style={{ color: "red" }}>{errors.email}</AppText>
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
@@ -35,10 +43,8 @@ function LoginScreen(props) {
               secureTextEntry
               textContentType="password"
             />
-            <AppButton
-              title="Login"
-              onPress={handleSubmit}
-            />
+            <AppText style={{ color: "red" }}>{errors.password}</AppText>
+            <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
       </Formik>
