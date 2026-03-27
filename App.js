e7c23031889f519dcd884f795fc9c641b65e40a2
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-27 08:45:28 -0400

    Build draft of ImageInput component

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
+import * as ImagePicker from "expo-image-picker";
+import { useEffect, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Button from "./app/components/Button";
+import ImageInput from "./app/components/ImageInput";
+import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.log("Error reading image ", error);
+    }
+  };
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) {
+      alert("You need to enable permission to access the library.");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       {
-        <WelcomeScreen />
+        <Screen>
+          <Button title="select" onPress={selectImage}/>
+          <ImageInput imageUri={imageUri}/>
+        </Screen>
       }
     </GestureHandlerRootView>
   );
