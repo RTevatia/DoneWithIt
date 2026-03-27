8b6d8d6badee76269b1fa0f6ebe96736ee0fd361
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-27 09:18:33 -0400

    refactor: add interactive ImageInput with selection delete and permissions

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
-import * as ImagePicker from "expo-image-picker";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import Button from "./app/components/Button";
 import ImageInput from "./app/components/ImageInput";
 import Screen from "./app/components/Screen";
 
 export default function App() {
   const [imageUri, setImageUri] = useState();
 
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-
-      if (!result.canceled) {
-        setImageUri(result.assets[0].uri);
-      }
-    } catch (error) {
-      console.log("Error reading image ", error);
-    }
-  };
-
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) {
-      alert("You need to enable permission to access the library.");
-    }
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       {
         <Screen>
-          <Button title="select" onPress={selectImage}/>
-          <ImageInput imageUri={imageUri}/>
+          <ImageInput
+            imageUri={imageUri}
+            onChangeImage={(uri) => setImageUri(uri)}
+          />
         </Screen>
       }
     </GestureHandlerRootView>
