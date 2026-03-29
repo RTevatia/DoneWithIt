2892bd81a8f247b0137646bbbcb1206bfadd46c4
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-29 07:06:59 -0400

    feat: add image validation and FormImagePicker to listing edit form

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
-import { useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import ImageInputList from "./app/components/ImageInputList";
-import Screen from "./app/components/Screen";
+import ListEditScreen from "./app/screens/ListEditScreen";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
+  
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       {
-        <Screen>
-          <ImageInputList
-            imageUris={imageUris}
-            onAddImage={handleAdd}
-            onRemoveImage={handleRemove}
-          />
-        </Screen>
+        <ListEditScreen />
       }
     </GestureHandlerRootView>
   );
