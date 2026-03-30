5b27750795dc1fcd5be968702fb7121ab3299d43
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-30 07:03:55 -0400

    feat: add custom navigation theme for React Navigation

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       {
-        <NavigationContainer>
+        <NavigationContainer theme={navigationTheme}>
           <AuthNavigator />
         </NavigationContainer>
       }
