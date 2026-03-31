e21d09a951713569039f4c1b7ef382d735430320
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-31 07:10:08 -0400

    feat: fetch listings from API and update UI with dynamic data

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import Card from "../components/Card";
-import Screen from "../components/Screen";
+import listingsApi from "../api/listings";
 import routes from "../navigation/routes";
-
-const listings = [
-  {
-    id: 1,
-    title: "Red jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
+import Screen from "../components/Screen";
 
 function ListingScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings(), []
+  });
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+
+    if (!response.ok) {
+      console.log("Error fetching listings");
+      return;
+    }
+
+    setListings(response.data)
+  }
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -30,7 +33,7 @@ function ListingScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
