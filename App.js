62005ec07799485e9d58dd4022beeac209ee8fe8
Rahul Tevatia <rtevatia2@gmail.com>
2026-04-02 10:29:22 -0400

    feat(cache): implement async storage cache layer with expiration handling

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import { NavigationContainer } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import AppNavigator from "./app/navigation/AppNavigator";
-import navigationTheme from "./app/navigation/navigationTheme";
-
 export default function App() {
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.stringify(value);
+
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  demo();
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      {
-        <NavigationContainer theme={navigationTheme}>
-          <AppNavigator />
-        </NavigationContainer>
-      }
-    </GestureHandlerRootView>
+    <GestureHandlerRootView style={{ flex: 1 }}>{}</GestureHandlerRootView>
   );
 }
